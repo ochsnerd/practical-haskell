5bc84f9d7e2b9ee35de64eb49d1af73e54bdd1b3
David Ochsner <davidochsner93@gmail.com>
2025-01-25 09:22:29 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "time-machines";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = inputs:
+    let
+      overlay = final: prev: {
+        haskell = prev.haskell // {
+          packageOverrides = hfinal: hprev:
+            prev.haskell.packageOverrides hfinal hprev // {
+              time-machines = hfinal.callCabal2nix "time-machines" ./. { };
+            };
+        };
+        time-machines = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.time-machines;
+      };
+      perSystem = system:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; };
+          hspkgs = pkgs.haskellPackages;
+        in
+        {
+          devShells = rec {
+            default = time-machines-shell;
+            time-machines-shell = hspkgs.shellFor {
+              withHoogle = true;
+              packages = p: [ p.time-machines ];
+              buildInputs = [
+                hspkgs.cabal-install
+                hspkgs.haskell-language-server
+                hspkgs.hlint
+                hspkgs.ormolu
+                pkgs.bashInteractive
+              ];
+            };
+          };
+          packages = rec {
+            default = time-machines;
+            time-machines = pkgs.time-machines;
+          };
+        };
+    in
+    { inherit overlay; } // inputs.flake-utils.lib.eachDefaultSystem perSystem;
+}
